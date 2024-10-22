7802c0171d710356646758c9d747511f6a6ae399
Ibrahima dieng <lindex171@gmail.com>
2024-10-22 13:08:58 +0000

    housing loation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    HomeComponent
+    HomeComponent,
+    HousingLocationComponent
   ],
   template: `
   <main>
